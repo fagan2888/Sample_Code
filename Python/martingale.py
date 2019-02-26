62eebd0a56d47eac6288ef77e1f911e9faa159a6
Shi, Shijie <shijie.shi@gatech.edu>
2019-02-26 17:20:29 -0500

    Update Python

--- Python/martingale.py
+++ Python/martingale.py
@@ -21,30 +21,83 @@ GT honor code violation.
 -----do not edit anything above this line--- 			  		 			     			  	   		   	  			  	
  			  		 			     			  	   		   	  			  	
 Student Name: Tucker Balch (replace with your name) 			  		 			     			  	   		   	  			  	
-GT User ID: tb34 (replace with your User ID) 			  		 			     			  	   		   	  			  	
-GT ID: 900897987 (replace with your GT ID) 			  		 			     			  	   		   	  			  	
+GT User ID: sshi48 (replace with your User ID) 			  		 			     			  	   		   	  			  	
+GT ID: 902860768 (replace with your GT ID) 			  		 			     			  	   		   	  			  	
 """ 			  		 			     			  	   		   	  			  	
  			  		 			     			  	   		   	  			  	
-import numpy as np 			  		 			     			  	   		   	  			  	
+import numpy as np 		  		 			     			  	   		   	  			  	
  			  		 			     			  	   		   	  			  	
-def author(): 			  		 			     			  	   		   	  			  	
-        return 'tb34' # replace tb34 with your Georgia Tech username. 			  		 			     			  	   		   	  			  	
+def author():
+    return 'sshi48' # replace tb34 with your Georgia Tech username. 			  		 			     			  	   		   	  			  	
  			  		 			     			  	   		   	  			  	
-def gtid(): 			  		 			     			  	   		   	  			  	
-	return 900897987 # replace with your GT ID number 			  		 			     			  	   		   	  			  	
+def gtid():
+    return 902860768 # replace with your GT ID number 			  		 			     			  	   		   	  			  	
  			  		 			     			  	   		   	  			  	
-def get_spin_result(win_prob): 			  		 			     			  	   		   	  			  	
-	result = False 			  		 			     			  	   		   	  			  	
-	if np.random.random() <= win_prob: 			  		 			     			  	   		   	  			  	
-		result = True 			  		 			     			  	   		   	  			  	
-	return result 			  		 			     			  	   		   	  			  	
+def get_spin_result(win_prob):
+    result = False
+    if np.random.random() <= win_prob: 			  		 			     			  	   		   	  			  	
+		result = True
+    return result 			  		 			     			  	   		   	  			  	
  			  		 			     			  	   		   	  			  	
-def test_code(): 			  		 			     			  	   		   	  			  	
-	win_prob = 0.60 # set appropriately to the probability of a win 			  		 			     			  	   		   	  			  	
-	np.random.seed(gtid()) # do this only once 			  		 			     			  	   		   	  			  	
-	print get_spin_result(win_prob) # test the roulette spin 			  		 			     			  	   		   	  			  	
- 			  		 			     			  	   		   	  			  	
-	# add your code here to implement the experiments 			  		 			     			  	   		   	  			  	
- 			  		 			     			  	   		   	  			  	
-if __name__ == "__main__": 			  		 			     			  	   		   	  			  	
-    test_code() 			  		 			     			  	   		   	  			  	
+def nolimit(winnings):
+    win_prob = 0.47 # set appropriately to the probability of a win
+    #np.random.seed(gtid()) # do this only once
+    #print get_spin_result(win_prob) # test the roulette spin
+    
+    #add your code here to implement the experiments
+    i = 0 
+    winnings.append(0)
+    
+    while winnings[i] < 80 and i <=1000:      
+        won = False
+        bet_amount = 1
+        while won == False and i <=1000:
+            won = get_spin_result(win_prob)
+            i = i + 1
+            if won == True:
+                winnings1.append( winnings[i-1] + bet_amount )
+            else:
+                winnings1.append( winnings[i-1] - bet_amount )
+                bet_amount = bet_amount * 2  
+    
+    if i < 1000:
+        for j in range(i, 1000):
+            winnings.append(80)
+            j = j + 1
+
+
+def limit(winnings):
+    win_prob = 0.47
+    i = 0 
+    winnings.append(0)
+    
+    while winnings[i] < 80 and winnings[i] > -256 and i <=1000:      
+        won = False
+        bet_amount = 1
+        
+        while won == False and winnings[i] > -256 and i <=1000:
+            won = get_spin_result(win_prob)
+            i = i + 1
+            if won == True:
+                winnings.append( winnings[i-1] + bet_amount )
+            else:
+                winnings.append( winnings[i-1] - bet_amount )
+                bet_amount = min( bet_amount * 2, 256 + winnings[i] )   
+
+    if i < 1000:
+        for j in range(i, 1000):
+            winnings.append(winnings[i])
+            j = j + 1       
+    	 			     			  	   		   	  			  		  		 			     			  	   		   	  			  	
+if __name__ == "__main__":
+    winnings1 = []
+    winnings2 = []
+    
+    nolimit(winnings1)
+    limit(winnings2)
+    
+    print winnings1
+    print winnings2
+
+      		 			     			  	   		   	  			  	
+
