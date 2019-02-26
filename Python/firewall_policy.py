7739f6d7010b104049c3a86f529fbbfacc5a6bf0
Shi, Shijie <shijie.shi@gatech.edu>
2019-02-26 08:26:35 -0500

    update Python folder

---
+++ Python/firewall_policy.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+"Assignment 5 - This creates the firewall policy. "
+
+from pyretic.lib.corelib import *
+from pyretic.lib.std import *
+from pyretic.lib.query import packets
+from pyretic.core.packet import IPV4, TCP_PROTO
+from collections import namedtuple
+
+Policy = namedtuple('Policy', ('srcmac', 'dstmac','srcip','dstip','srcport','dstport'))
+
+def make_firewall_policy(config):
+    # TODO - This is where you need to write the functionality to create the
+    # firewall. What is passed in is a list of rules that you must implement
+    # using the Pyretic syntax that was used in Assignment 2. 
+
+    # feel free to remove the following "print config" line once you no longer need it
+    #print "***Amy print config:"
+    #print config # for demonstration purposes only, so you can see the format of the config
+    firewall_policies = firewall_list(config)
+    #Deal with the case if there is not firewall policy
+    if (firewall_policies.__len__() < 1):
+        raise TypeError("No firewall policy\n")
+
+    rules = []
+    for policy in firewall_policies.itervalues():
+        # TODO - build the individual rules
+        rule = firewall_rule(policy)
+        rules.append(rule)
+        pass
+
+    allowed = ~(union(rules))
+
+    print "Allowed:\n"
+    print allowed 
+   
+    return allowed
+
+def firewall_list(config):
+    policy_list = {}
+    for entry in config:
+        policy_list [entry['rulenum']] = Policy(entry['srcmac'], entry['dstmac'],
+                                                entry['srcip'], entry['dstip'],
+                                                entry['srcport'], entry['dstport'])
+        values = policy_list.values()
+    return policy_list
+
+def firewall_rule(policy):
+    #default 
+    rule = match(ethtype=IPV4, protocol=TCP_PROTO)
+
+    if (block_all(policy) == true):
+        rule = match(ethtype=IPV4, protocol=TCP_PROTO)
+    else:
+        #Check srcmac,dstmac,dstport,srcport,srcip,dstip,srcip
+        if ((policy.srcmac != '*') and (policy.dstmac != '*')):
+            rule = match(srcmac = EthAddr(policy.srcmac), dstmac = EthAddr(policy.dstmac))
+        elif (policy.srcmac != '*'):
+            rule = match(srcmac = EthAddr(policy.srcmac))
+        elif (policy.dstmac != '*'):
+            rule = match(dstmac = EthAddr(policy.dstmac))
+        if (policy.dstport != '*'):
+            rule = rule & match(dstport = int(policy.dstport))
+        if (policy.srcport != '*'):
+            rule = rule & match(srcport = int(policy.srcport))
+        if ((policy.srcip != '*') and (policy.dstip != '*')):
+            rule = rule & match(srcip = IPAddr(policy.srcip), dstip=IPAddr(policy.dstip))
+        elif (policy.dstip != '*'):
+            rule = rule & match(dstip = IPAddr(policy.dstip))
+        elif (policy.srcip != '*'):
+            rule = rule & match(dstip = IPAddr(policy.srcip))
+
+    return rule
+
+
+
+
+def block_all(policy):
+    if (policy.srcmac == '*' and policy.dstmac == '*' \
+        and policy.srcip == '*' and policy.dstip == '*' \
+        and policy.srcport == '*' and policy.dstport == '*'):
+        print "Block all ports"
+        return True
+    else:
+        return False
